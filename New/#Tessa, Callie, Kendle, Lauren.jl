aa0c6c242ebde2bb94681aab99c0237b71b7980c
tessathompson0 <114413185+tessathompson0@users.noreply.github.com>
2023-09-21 09:29:24 -0500

    Create #Tessa, Callie, Kendle, Lauren.jl

---
+++ New/#Tessa, Callie, Kendle, Lauren.jl
@@ -0,0 +1,74 @@
+#Tessa, Callie, Kendle, Lauren
+#9.21.23
+#Importing and defining Winsound
+import winsound
+
+# head, shoulders, knees, and toes, knees and toes.
+def part1():
+    #G
+    winsound.Beep(392,200) 
+    #A
+    winsound.Beep(440,200)
+    #G
+    winsound.Beep(392,200)
+    #F#
+    winsound.Beep(370,200)
+    #G
+    winsound.Beep(392,200)
+    #E
+    winsound.Beep(329,200)
+    #G
+    winsound.Beep(392,200)
+    #G
+    winsound.Beep(392,200)
+    #G
+    winsound.Beep(392,200)
+    #G
+    winsound.Beep(392,200)
+    #A
+    winsound.Beep(440,200)
+    #G
+    winsound.Beep(392,200)
+    #F#
+    winsound.Beep(370,200)
+    #G
+    winsound.Beep(392,200)
+    #D
+    winsound.Beep(146,200)
+    #G
+    winsound.Beep(392,200)
+    #G
+    winsound.Beep(392,200)
+    #G
+    winsound.Beep(392,200)
+    #f
+    winsound.Beep(349,200)
+    #E
+    winsound.Beep(329,200)
+    #D
+    winsound.Beep(146,200)
+  
+def part2():
+    #Low C
+    winsound.Beep(130,200)
+    #E
+    winsound.Beep(329,200)
+    #G
+    winsound.Beep(392,200)
+    #High C
+    winsound.Beep(261,200)
+    #G
+    winsound.Beep(587,200)
+    #High C
+    winsound.Beep(261,200)
+    #High B
+    winsound.Beep(246,200)
+    #High C
+    winsound.Beep(261,200)
+    #A
+    winsound.Beep(220,200)
+
+part1() 
+part2()
+part1()
+part2()
